1145fae700aede97e91dc5c0f80165c3ce8daffc
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-08-04 12:13:00 +0530

    🎉 created profile card with a sharp shadow

---
+++ profile-card-sharp-shadow/index.js
@@ -0,0 +1,28 @@
+import { setup, strict } from "https://cdn.skypack.dev/twind/shim";
+import * as colors from "https://cdn.skypack.dev/twind/colors";
+
+setup({
+    hash: false,
+    darkMode: false,
+    mode: strict,
+    theme: {
+        fontFamily: {
+            body: ['Inter', 'sans-serif']
+        },
+        container: {
+            center: true
+        },
+        colors: {
+            gray: colors.blueGray,
+            primary: '#1e90ff',
+            white: '#FFF'
+        },
+        boxShadow: {
+            'card': '0px 15px 0px -5px rgba(0, 31, 61, 0.1)'
+        },
+        backgroundImage: {
+            'html':
+                'linear-gradient(90deg, rgba(235,245,255,1) 0%, rgba(214,235,255,1) 100%)',
+        }
+    }
+});
