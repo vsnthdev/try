f8380fe05c82485b7643103fd61cba9e07edc808
Vasanth Developer <vsnthdev@gmail.com>
2021-11-04 05:07:57 +0530

    ✨ | created buttons

---
+++ brand-buttons/index.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            body: ['Basically A Sans Serif', 'sans-serif']
+        },
+        container: {
+            center: true
+        },
+        extend: {
+            colors: {
+                primary: '#006CFF',
+                'primary-alt': '#D6E7FF',
+                secondary: '#000000',
+                'secondary-alt': '#E2E8F0'
+            }
+        }
+    }
+}
