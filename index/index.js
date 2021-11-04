570558692ec7881cc958842b4b2f42567cdb6276
Vasanth Developer <vsnthdev@gmail.com>
2021-11-04 18:28:54 +0530

    ✨ | created an indexing service

---
+++ index/index.js
@@ -0,0 +1,29 @@
+import axios from 'https://cdn.skypack.dev/axios'
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/rest'
+
+const octokit = new Octokit()
+
+const { data } = await octokit.rest.repos.getContent({
+    owner: 'vsnthdev',
+    repo: 'try'
+})
+
+for (const folder of data) {
+    if (folder.name == '.gitignore') continue
+
+    const { data:contents } = await axios({
+        method: 'GET',
+        url: `https://raw.githubusercontent.com/vsnthdev/try/main/${folder.name}/index.html`
+    })
+
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(contents, 'text/html')
+
+    const title = doc.querySelector('title').textContent
+
+    const anchor = document.createElement('a')
+    anchor.textContent = title
+    anchor.href = `https://try.vsnth.dev/${folder.name}`
+
+    document.body.appendChild(anchor)
+}
