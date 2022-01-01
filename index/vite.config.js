e5fe85a2d7744965c0bfcbc2cbad9292d51f5a1e
Vasanth Developer <vsnthdev@gmail.com>
2022-01-01 18:32:25 +0530

    🔨 building now works for index project

---
+++ index/vite.config.js
@@ -0,0 +1,14 @@
+/*
+ *  Vite.js bundler config for index project.
+ *  Created On 01 January 2022
+ */
+
+import merge from 'deepmerge';
+import { defineConfig } from 'vite';
+import base from '../vite.config.js';
+
+export default merge(base, defineConfig({
+    build: {
+        outDir: '../dist'
+    }
+}))
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/*
+ *  Global Vite.js configuration used across all sub-projects.
+ *  Created On 01 January 2022
+ */
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    clearScreen: false,
+    publicDir: '../public',
+    build: {
+        emptyOutDir: true
+    },
+    server: {
+        fs: {
+            strict: false
+        }
+    }
+})
