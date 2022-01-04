2863b82708e1276da2def7c257dd25792489060e
Vasanth Developer <vsnthdev@gmail.com>
2022-01-04 13:28:12 +0530

    ⚡ caches are now cleared upon every new build

--- src/index/src/assets/js/sw.js
+++ src/index/src/assets/js/sw.js
@@ -3,8 +3,10 @@ import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate } from 'workbox-strategies'
 
-// pre-cache assets built using Vite.js
 precacheAndRoute(self.__WB_MANIFEST)
+
+// remove old preCache done by Vite.js & workbox
+cleanupOutdatedCaches()
 
 const libHosts = ['cdn.tailwindcss.com', 'tailwindcss.com', 'vyaktitva.vercel.app', 'cdn.skypack.dev', 'unpkg.com']
 registerRoute(
@@ -31,12 +33,10 @@ registerRoute(
     }),
 )
 
-// remove old preCache done by Vite.js & workbox
-cleanupOutdatedCaches()
-
 // manually remove old caches
-const obsoleteCaches = ['fonts', 'try', 'github-api']
-
-for (const cacheName of obsoleteCaches) {
-    caches.delete(cacheName)
-}
+self.addEventListener('activate', async () => {
+    const names = await caches.keys()
+    for (const name of names) {
+        if (name.includes('workbox') == false) await caches.delete(name)
+    }
+})
