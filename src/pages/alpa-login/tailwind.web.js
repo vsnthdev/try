edf4d2e8422fd933a919bd0a666115d7757cb463
Vasanth Developer <vsnthdev@gmail.com>
2022-02-06 20:10:44 +0530

    ✨ created alpa-login page

---
+++ src/pages/alpa-login/tailwind.web.js
@@ -0,0 +1,12 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                light: '#FEF2F2',
+                brand: '#EF3054',
+                dark: '#1C1917'
+            }
+        }
+    }
+}
