4f1993c4996c0ba0fbf88ca2ad03337598151e46
Vasanth Developer <vsnthdev@gmail.com>
2022-10-24 18:22:30 +0530

    ✨ created hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,23 @@ const Index: NextPage = ({}: InferGetStaticPropsType<typeof getStaticProps>) => 
         <NextSeo
             title={`Try outs - Vasanth Srivatsa`}
         />
+
+        {/* page container */}
+        <div className='container mx-auto'>
+            <div className='w-full max-w-5xl mx-auto px-7'>
+                {/* page title */}
+                <div className='pt-10 pb-10 md:pt-14 md:pb-14 lg:pt-20 lg:pb-14 xl:pt-24 xl:pb-16'>
+                    <h1 className='leading-normal text-3xl font-semibold sm:text-4xl sm:leading-normal xl:text-5xl xl:leading-normal'>
+                        <span className='text-slate-500 xl:text-4xl dark:text-white/60'>Playground where I </span><br className='hidden md:inline' />
+                        <span className='font-bold'>🥇 practice, ⚗️ experiment & ✨ create </span><br className='hidden lg:inline' />
+                        <span className='text-slate-500 xl:text-4xl dark:text-white/60'>different components and pages.</span>
+                    </h1>
+                </div>
+
+                {/* cards for different elements */}
+                <div></div>
+            </div>
+        </div>
     </>
 }
 
