eb29010add368ff2c22c33d4ba82e9a3f36a8efe
Vasanth Developer <vsnthdev@gmail.com>
2023-03-30 23:58:12 +0530

    ⚡ moved projects from StackBlitz to GitHub Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,21 +9,34 @@ import { camelCase } from 'change-case'
 import { Client } from '@notionhq/client'
 import { NextPage, InferGetStaticPropsType } from 'next'
 
-const transformBlitz = blitzProperties => {
+interface Transformed {
+    type: string
+    preview: string
+    title: string
+}
+
+const transform = props => {
     const returnable = {}
 
-    for (const oldKey in blitzProperties) {
-        const obj = blitzProperties[oldKey]
+    for (const oldKey in props) {
+        const obj = props[oldKey]
         const key = camelCase(oldKey)
 
-        if (obj.type == 'select') {
-            returnable[key] = obj[obj.type].name
+        if (obj.type == 'title') {
+            returnable[key] = obj.title[0].plain_text
+        } else if (obj.type == 'url') {
+            returnable[key] = obj.url
+        } else if (obj.type == 'rich_text') {
+            if (obj.rich_text.length == 0) continue
+            returnable[key] = obj.rich_text[0].plain_text
+        } else if (obj.type == 'select') {
+            returnable[key] = obj.select.name
         } else {
-            returnable[key] = obj[obj.type][0].plain_text
+            console.log(`Found new type "${obj.type}" not implemented by transform() function`)
         }
     }
 
-    return returnable
+    return returnable as Transformed
 }
 
 export const getStaticProps = async () => {
@@ -40,12 +53,12 @@ export const getStaticProps = async () => {
     return {
         revalidate: 60,
         props: {
-            blitz: results.map((res: any) => transformBlitz(res.properties))
+            projects: results.map((res: any) => transform(res.properties))
         }
     }
 }
 
-const Index: NextPage = ({ blitz }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Index: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStaticProps>) => {
     return <>
         {/* page SEO */}
         <NextSeo
@@ -70,13 +83,13 @@ const Index: NextPage = ({ blitz }: InferGetStaticPropsType<typeof getStaticProp
 
                 {/* cards for different elements */}
                 <div className='flex flex-col space-y-12 pb-14 md:pb-24 lg:pb-32'>
-                    {blitz.map((stack: any) => <article key={stack.stackBlitzId} className='bg-gradient-to-r from-slate-900 to-slate-800/70 backdrop-blur-sm select-none rounded-xl shadow-2xl shadow-black/[0.1] p-8 flex flex-col justify-between md:flex-row focus:outline-none'>
+                    {projects.map(project => <article key={project.title} className='bg-gradient-to-r from-slate-900 to-slate-800/70 backdrop-blur-sm select-none rounded-xl shadow-2xl shadow-black/[0.1] p-8 flex flex-col justify-between md:flex-row focus:outline-none'>
                         <div>
-                            <span className='text-sm font-black uppercase tracking-widest opacity-30'>{stack.type}</span>
-                            <h3 className='text-3xl font-semibold mt-2'>{stack.title}</h3>
+                            <span className='text-sm font-black uppercase tracking-widest opacity-30'>{project.type}</span>
+                            <h3 className='text-3xl font-semibold mt-2'>{project.title}</h3>
                         </div>
                         <div className='mt-6 flex space-x-4'>
-                            <a href={`https://stackblitz.com/edit/${stack.stackBlitzId}?embed=1&hideExplorer=1&hideNavigation=1&hideDevTools=1&view=preview`} rel='noopener' target='_blank' className='bg-slate-700 p-3 rounded-xl'>
+                            <a href={project.preview} rel='noopener' target='_blank' className='bg-slate-700 p-3 rounded-xl'>
                                 <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6 pointer-events-none'>
                                     <path strokeLinecap='round' strokeLinejoin='round' d='M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59'></path>
                                 </svg>
